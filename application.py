853a2133ff25ad3f44d4fef53614a0d9bc6f2e15
yutatanamoto <y.tanamoto.152@gmail.com>
2021-01-20 15:34:33 +0900

    :hammer: modify message text

--- application.py
+++ application.py
@@ -87,16 +87,16 @@ def handle_message(event):
             }
         ]
         log_obj.put(Body = json.dumps(log_json, indent=4))
-        message = TextSendMessage(text="眠気レベルは{}だね!!!!記録したよ〜".format(message_text))
+        message = TextSendMessage(text="眠気レベルは{}ですね。\n記録させていただきました。".format(message_text))
         line_bot_api.push_message(user_id, message)
     else:
         running_status_byte_obj = running_status_obj.get()['Body'].read().decode('utf-8')
         running_status_json_obj = json.loads(running_status_byte_obj)
         running = running_status_json_obj["running"]
         if running:
-            message = TextSendMessage(text="眠気レベルは1~5のいずれかで回答してください〜\n回答を終了する場合は「終了」と送ってください〜")
+            message = TextSendMessage(text="眠気レベルは0~3のいずれかで回答してください。\n回答を終了する場合は「終了」と送ってください。")
         else:
-            message = TextSendMessage(text="回答を始める場合は「開始」と送ってね〜")
+            message = TextSendMessage(text="回答を始める場合は「開始」と送ってくださいね。")
         line_bot_api.push_message(user_id, message)
 
 def exists_s3_obj_key(key):
