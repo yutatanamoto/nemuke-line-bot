26146d863298383484d87a88b61266f9b668baf3
yutatanamoto <y.tanamoto.152@gmail.com>
2021-01-21 12:17:21 +0900

    :hammer: debugging

--- application.py
+++ application.py
@@ -46,6 +46,8 @@ def handle_message(event):
     display_name = profile.display_name
     running_status_json_key = "running_status_{}.json".format(display_name)
     running_status_obj = s3.Object(AWS_S3_BUCKET_NAME, running_status_json_key)
+    running_status = {'running': False}
+    running_status_obj.put(Body = json.dumps(running_status, indent=4))
     # running_status_byte_obj = running_status_obj.get()['Body'].read().decode('utf-8')
     # running_status_json_obj = json.loads(running_status_byte_obj)
     # running = running_status_json_obj["running"]
