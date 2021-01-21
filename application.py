4e24976f38accb5b32343153c01e17dc9025c67e
yutatanamoto <y.tanamoto.152@gmail.com>
2021-01-21 14:03:57 +0900

    :hammer: update if statement condition

--- application.py
+++ application.py
@@ -78,7 +78,7 @@ def handle_message(event):
         running_status_obj.put(Body = json.dumps(running_status_json, indent=4))
         message = TextSendMessage(text="回答ありがとうございました!\nお疲れ様でした!!")
         line_bot_api.push_message(user_id, message)
-    elif message_text in "01" and running:
+    elif message_text in "眠い眠くない" and running:
         current_unix_time = time.time()
         log_json_key = "log_{}.json".format(display_name)
         log_obj = s3.Object(AWS_S3_BUCKET_NAME, log_json_key)
